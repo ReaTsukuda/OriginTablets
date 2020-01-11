60ea2cb1d6ac1f35d455b2aa74f21facffb4ca30
Rea <1171053+ReaTsukuda@users.noreply.github.com>
2020-01-11 10:56:26 -0800

    added tests for modified tables

--- OTTests/TableTests.cs
+++ OTTests/TableTests.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OriginTablets.Types;
 using System.Linq;
+using System.IO;
 
 namespace OTTests
 {
@@ -13,21 +14,22 @@ namespace OTTests
     [TestMethod]
     public void CompareParsedTableWithKnownCorrectList()
     {
-      if (System.IO.File.Exists("Resources/EO3PSNT.tbl") == false)
+      if (File.Exists("Resources/EO3PSNT.tbl") == false)
       {
         throw new InternalTestFailureException("EO3PSNT.tbl is missing. Please copy a vanilla EO3 playerskillnametable.tbl to OTTests/Resources.");
       }
-      var exampleTable = new OriginTablets.Types.Table("Resources/EO3PSNT.tbl", false);
-      var knownCorrect = System.IO.File.ReadAllLines("Resources/EO3PSNT.txt");
+      var exampleTable = new Table("Resources/EO3PSNT.tbl", false);
+      var knownCorrect = File.ReadAllLines("Resources/EO3PSNT.txt");
       // If the table and known correct list differ in number of entries, something has gone very wrong.
       Assert.AreEqual(exampleTable.Count, knownCorrect.Length);
-      // If the prior check passed, we can iterate based on a numeric index, 
+      // If the prior check passed, we can iterate based on a numeric index,
       // and make sure every string is the same.
       for (int index = 0; index < exampleTable.Count; index += 1)
       {
         Assert.AreEqual(exampleTable[index], knownCorrect[index]);
       }
     }
+
     /// <summary>
     /// Checks the results of parsing a vanilla EO3 skillcustomtable.tbl with a known correct output.
     /// This is to test long pointer tables.
@@ -35,15 +37,15 @@ namespace OTTests
     [TestMethod]
     public void CompareParsedLongPointerTableWithKnownCorrectList()
     {
-      if (System.IO.File.Exists("Resources/EO3SCT.tbl") == false)
+      if (File.Exists("Resources/EO3SCT.tbl") == false)
       {
         throw new InternalTestFailureException("EO3SCT.tbl is missing. Please copy a vanilla EO3 skillcustomtable.tbl to OTTests/Resources.");
       }
-      var exampleTable = new OriginTablets.Types.Table("Resources/EO3SCT.tbl", true);
-      var knownCorrect = System.IO.File.ReadAllLines("Resources/EO3SCT.txt");
+      var exampleTable = new Table("Resources/EO3SCT.tbl", true);
+      var knownCorrect = File.ReadAllLines("Resources/EO3SCT.txt");
       // If the table and known correct list differ in number of entries, something has gone very wrong.
       Assert.AreEqual(exampleTable.Count, knownCorrect.Length);
-      // If the prior check passed, we can iterate based on a numeric index, 
+      // If the prior check passed, we can iterate based on a numeric index,
       // and make sure every string is the same.
       for (int index = 0; index < exampleTable.Count; index += 1)
       {
@@ -58,16 +60,16 @@ namespace OTTests
     [TestMethod]
     public void CompareWrittenTableWithVanillaTable()
     {
-      if (System.IO.File.Exists("Resources/EO3PSNT.tbl") == false)
+      if (File.Exists("Resources/EO3PSNT.tbl") == false)
       {
         throw new InternalTestFailureException("EO3PSNT.tbl is missing. Please copy a vanilla EO3 playerskillnametable.tbl to OTTests/Resources.");
       }
-      var tableObject = new OriginTablets.Types.Table("Resources/EO3PSNT.tbl", false);
+      var tableObject = new Table("Resources/EO3PSNT.tbl", false);
       tableObject.WriteToFile("Resources/EO3PSNTTemp.tbl", false);
-      var originalBytes = System.IO.File.ReadAllBytes("Resources/EO3PSNT.tbl");
-      var newBytes = System.IO.File.ReadAllBytes("Resources/EO3PSNTTemp.tbl");
+      var originalBytes = File.ReadAllBytes("Resources/EO3PSNT.tbl");
+      var newBytes = File.ReadAllBytes("Resources/EO3PSNTTemp.tbl");
       Assert.IsTrue(originalBytes.SequenceEqual(newBytes));
-      System.IO.File.Delete("Resources/EO3PSNTTemp.tbl");
+      File.Delete("Resources/EO3PSNTTemp.tbl");
     }
 
     /// <summary>
@@ -77,16 +79,64 @@ namespace OTTests
     [TestMethod]
     public void CompareWrittenLongPointerTableWithVanillaTable()
     {
-      if (System.IO.File.Exists("Resources/EO3SCT.tbl") == false)
+      if (File.Exists("Resources/EO3SCT.tbl") == false)
       {
-        throw new InternalTestFailureException("EO3SCT.tbl is missing. Please copy a vanilla EO3 playerskillnametable.tbl to OTTests/Resources.");
+        throw new InternalTestFailureException("EO3SCT.tbl is missing. Please copy a vanilla EO3 skillcustomtable.tbl to OTTests/Resources.");
       }
-      var tableObject = new OriginTablets.Types.Table("Resources/EO3SCT.tbl", true);
+      var tableObject = new Table("Resources/EO3SCT.tbl", true);
       tableObject.WriteToFile("Resources/EO3SCTTemp.tbl", true);
-      var originalBytes = System.IO.File.ReadAllBytes("Resources/EO3SCT.tbl");
-      var newBytes = System.IO.File.ReadAllBytes("Resources/EO3SCTTemp.tbl");
+      var originalBytes = File.ReadAllBytes("Resources/EO3SCT.tbl");
+      var newBytes = File.ReadAllBytes("Resources/EO3SCTTemp.tbl");
       Assert.IsTrue(originalBytes.SequenceEqual(newBytes));
-      System.IO.File.Delete("Resources/EO3SCTTemp.tbl");
+      File.Delete("Resources/EO3SCTTemp.tbl");
+    }
+
+    /// <summary>
+    /// Checks that the result of writing a Table parsed from a vanilla EO3 playerskillnametable.tbl
+    /// and then modifying its string at index 4 to "MODIFIED NAME" results in a file that is identical
+    /// to a known working table with the same modification made.
+    /// </summary>
+    [TestMethod]
+    public void ModifyTableThenCompareToKnownWorkingModifiedTable()
+    {
+      if (File.Exists("Resources/EO3PSNT.tbl") == false)
+      {
+        throw new InternalTestFailureException("EO3PSNT.tbl is missing. Please copy a vanilla EO3 playerskillnametable.tbl to OTTests/Resources.");
+      }
+      if (File.Exists("Resources/EO3PSNTModified.tbl") == false)
+      {
+        throw new InternalTestFailureException("EO3PSNTModified.tbl is missing. Please copy a EO3 playerskillnametable.tbl with the string at index 4 modified to \"MODIFIED NAME\" to OTTests/Resources.");
+      }
+      var parsedVanillaTable = new Table("Resources/EO3PSNT.tbl", false);
+      parsedVanillaTable[4] = "MODIFIED NAME";
+      parsedVanillaTable.WriteToFile("Resources/EO3PSNTTemp.tbl", false);
+      var testedBytes = File.ReadAllBytes("Resources/EO3PSNTTemp.tbl");
+      var knownWorkingBytes = File.ReadAllBytes("Resources/EO3PSNTModified.tbl");
+      Assert.IsTrue(testedBytes.SequenceEqual(knownWorkingBytes));
+    }
+
+    /// <summary>
+    /// Checks that the result of writing a Table parsed from a vanilla EO3 skillcustomtable.tbl
+    /// and then modifying its string at index 23 to "MODIFIED LEVEL UP" results in a file that is identical
+    /// to a known working table with the same modification made. This is to test long pointer table modifications.
+    /// </summary>
+    [TestMethod]
+    public void ModifyLongPointerTableThenCompareToKnownWorkingModifiedTable()
+    {
+      if (File.Exists("Resources/EO3SCT.tbl") == false)
+      {
+        throw new InternalTestFailureException("EO3SCT.tbl is missing. Please copy a vanilla EO3 skillcustomtable.tbl to OTTests/Resources.");
+      }
+      if (File.Exists("Resources/EO3SCTModified.tbl") == false)
+      {
+        throw new InternalTestFailureException("EO3SCTModified.tbl is missing. Please copy a EO3 skillcustomtable.tbl with the string at index 4 modified to \"MODIFIED NAME\" to OTTests/Resources.");
+      }
+      var parsedVanillaTable = new Table("Resources/EO3SCT.tbl", true);
+      parsedVanillaTable[23] = "MODIFIED LEVEL UP";
+      parsedVanillaTable.WriteToFile("Resources/EO3SCTTemp.tbl", true);
+      var testedBytes = File.ReadAllBytes("Resources/EO3SCTTemp.tbl");
+      var knownWorkingBytes = File.ReadAllBytes("Resources/EO3SCTModified.tbl");
+      Assert.IsTrue(testedBytes.SequenceEqual(knownWorkingBytes));
     }
   }
 }
